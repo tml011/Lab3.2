fa1a1d6f76915818c5461832e06736d7bf514378
tml <vvv.sharipov.vvv@gmail.com>
2026-04-17 19:35:51 +0500

    Написал реализацию конструктора, методов класса

--- model.cpp
+++ model.cpp
@@ -16,3 +16,45 @@ void Model::save()
     settings->setValue("valueC", c);
 
 }
+
+void Model::setA(int value)
+{
+    if (value < 0) value = 0;
+    if (value > 100) value = 100;
+
+    if (this ->a == value) return;
+
+    this->a = value;
+    if (a > b) b = a;
+    if (b > c) c = b;
+
+    save();
+    emit dataChanged();
+}
+
+void Model::setB(int value)
+{
+    if (value < a) value = a;
+    if (value > c) value = c;
+
+    if (this->b == value) return;
+    this->b = value;
+
+    save();
+    emit dataChanged();
+}
+
+void Model::setC(int value)
+{
+    if (value < 0) value = 0;
+    if (value > 100) value = 100;
+
+    if (this->c == value) return;
+
+    this->c = value;
+    if (c < b) b = c;
+    if (b < a) a = c;
+
+    save();
+    emit dataChanged();
+}
